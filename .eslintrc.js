41f3967d99ca0dbdae35cf911cca52b7bb7175ac
babramczyk <bjabramczyk@gmail.com>
2020-02-21 08:59:21 -0600

    add TS to eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,8 @@
 module.exports = {
-  // Dep names: 'eslint-plugin-jest', 'eslint-plugin-react', 'eslint-plugin-react-hooks'
+  // Dep names: 'eslint-plugin-jest', 'eslint-plugin-react', 'eslint-plugin-react-hooks', '@typescript-eslint/eslint-plugin', '@typescript-eslint/parser'
 
   // REVIEW: React plugins and rules
+  // REVIEW: TypeScript rules/config (file extensions, plugins, parsers, etc.)
 
   plugins: ['react-hooks'],
   extends: ['plugin:react/recommended'],
@@ -119,7 +120,7 @@ module.exports = {
      */
     {
       // REVIEW: Jest setup file, test utils, etc. Anything that's not a `spec` file that should be treated in the test env
-      files: ['client/**/*.spec.@(js|jsx)', './jest.setup.js', 'client/test-utils/**/*.js'],
+      files: ['client/**/*.spec.@(js|jsx|ts|tsx)', './jest.setup.js', 'client/test-utils/**/*.(js|jsx|ts|tsx)'],
       extends: ['plugin:jest/recommended'],
       plugins: ['jest'],
       env: {
@@ -129,6 +130,46 @@ module.exports = {
       },
       rules: {
         'jest/prefer-to-have-length': 2,
+      },
+    },
+
+    /**
+     * TypeScript files.
+     */
+    {
+      files: ['**/*+(ts|tsx)'],
+      parser: '@typescript-eslint/parser',
+      parserOptions: {
+        project: './tsconfig.json',
+      },
+      plugins: ['@typescript-eslint'],
+      rules: {
+        // Turn off rules that TypeScript's compiler can/will catch on its own. This is necessary, since this project has a mix of JS and TS files.
+        // REVIEW: Make sure the TS compiler enforces these rules on its own before comitting to turning these ESLint rules off
+        'no-unused-vars': 0,
+        'no-use-before-define': 0,
+        'no-undef': 0,
+
+        // `@typescript-eslint` rules. The default recommended rules are a bit picky, and included useless warnings instead of error. So we explicitly choose the ones that could actually help catch bugs, reasonably enforce consistency, or reasonably enforce modern TS
+        '@typescript-eslint/await-thenable': 2,
+        '@typescript-eslint/ban-types': 2,
+        '@typescript-eslint/consistent-type-assertions': 2,
+        '@typescript-eslint/no-explicit-any': 2,
+        '@typescript-eslint/no-for-in-array': 2,
+        '@typescript-eslint/no-inferrable-types': 2,
+        '@typescript-eslint/no-misused-new': 2,
+        '@typescript-eslint/no-misused-promises': 2,
+        '@typescript-eslint/no-namespace': 2,
+        '@typescript-eslint/no-this-alias': 2,
+        '@typescript-eslint/no-unnecessary-type-assertion': 2,
+        '@typescript-eslint/no-var-requires': 2,
+        '@typescript-eslint/prefer-includes': 2,
+        '@typescript-eslint/prefer-namespace-keyword': 2,
+        '@typescript-eslint/prefer-string-starts-ends-with': 2,
+        '@typescript-eslint/triple-slash-reference': 2,
+        '@typescript-eslint/unbound-method': 2,
+        '@typescript-eslint/no-array-constructor': 2,
+        '@typescript-eslint/require-await': 2,
       },
     },
   ]
